842be25fca23db808b31e3d7c56aae0a49190b02
parkchanjong <7000cj@gmail.com>
2021-08-02 22:22:16 +0900

    [search] union find
    - 합집합 찾기

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,24 +1,19 @@
 #include<iostream>
-
 using namespace std;
 
-int n, m;
-int a[10001];
+int n;
 
-int main() {
-  scanf("%d", &n);
-  for (int i = 0; i < n; i++)
-  {
-    scanf("%d", &m);
-    a[m]++;
-  }
-  for (int i = 0; i < 10001; i++)
-  {
-    while (a[i] != 0)
-    {
-      printf("%d\n", i);
-      a[i]--;
-    }  
+int main(void) {
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      printf(" ");
+    }
+    for (int j = 0; j < n - i; j++) {
+      printf("*");
+    }
+    printf("\n");
   }
   return 0;
 }
