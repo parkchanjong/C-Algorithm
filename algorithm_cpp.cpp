ca95add10e6db952897288d3492bac25c6cb66b2
Park ChanJong <7000cj@gmail.com>
2022-03-13 20:25:43 +0900

    dijkstra

--- algorithm_cpp.cpp
+++ algorithm_cpp.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int N;
-    cin >> N;
-
-    int i = 1;
-    while (N > i)
-    {
-        N -= i;
-        i++;
-    }
-
-    if (i % 2 == 1)
-        cout << i + 1 - N << '/' << N << '\n';
-    else
-        cout << N << '/' << i + 1 - N << '\n';
+    int n;
+    cin >> n;
+    cout << n << endl;
+    return 0;
 }
---
+++ pro_algorithm_cpp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(int money, vector<int> costs)
+{
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    int a,b;
+    vector<int> ans;
+    cin >>a>>b;
+    costs.push_back(b);
+
+    cout << solution(0) << endl;
+    return 0;
+}
